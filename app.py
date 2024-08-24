9ec91c368fee155875958684e8fdee2cf47a616f
Joan Roa -joanroamora- en gitHub <joanroamora@gmail.com>
2024-08-24 01:27:38 -0500

    generando imagenes para analisis de datos

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import numpy as np
 import processing as proc
 
 def main():
     # Cargar el dataset
-    dfMusic = proc.load_dataset('./data/music.csv')
+    dfMusic = proc.load_dataset('data/music.csv')
     
     # Verificar valores nulos
     has_nulls = proc.check_null_values(dfMusic)
@@ -11,8 +12,10 @@ def main():
     # Plot inicial del boxplot
     proc.plot_initial_boxplot(dfMusic)
     
+    # Seleccionar solo las columnas numéricas
+    dfMusic_ft = dfMusic.select_dtypes(include=[np.number])
+
     # Escalar características
-    dfMusic_ft = dfMusic[dfMusic.columns[1:]]  # Excluyendo la primera columna que es el Track
     dfMusic_ftsc = proc.scale_features(dfMusic_ft)
     
     # Filtrar outliers
@@ -23,14 +26,14 @@ def main():
     proc.plot_filtered_boxplot(dfMusic_ftscClc)
     
     # Matriz de dispersión
-    features = list(dfMusic.columns[1:11])  # Cambiar según las características que te interesen
+    features = list(dfMusic_ft.columns[:10])  # Cambiar según las características que te interesen
     proc.plot_scatter_matrix(dfMusic, features, "Popularity")
     
     # Heatmap de correlación
     proc.plot_correlation_heatmap(dfMusic_ftscClc)
     
     # Coordenadas paralelas
-    proc.plot_parallel_coordinates(dfMusic_ftscClc.iloc[:, :150], "Popularity")
+    proc.plot_parallel_coordinates(dfMusic_ftscClc, "Popularity")
 
     # Curvas de Andrews
     proc.plot_andrews_curves(dfMusic_ftscClc, "Popularity")
--- processing.py
+++ processing.py
@@ -1,9 +1,14 @@
+import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from pandas.plotting import scatter_matrix, parallel_coordinates, andrews_curves
+
+# Asegúrate de que la carpeta ./outputs existe
+if not os.path.exists('./outputs'):
+    os.makedirs('./outputs')
 
 # Carga un dataset desde una ruta especificada
 def load_dataset(path):
@@ -18,7 +23,8 @@ def plot_initial_boxplot(df):
     plt.figure(figsize=(20,8))
     sns.boxplot(data=df)
     plt.xticks(rotation=90)
-    plt.show()
+    plt.savefig('./outputs/initial_boxplot.png')
+    plt.close()
 
 # Escala las características del DataFrame utilizando estandarización (media=0, desviación estándar=1)
 def scale_features(df):
@@ -36,7 +42,8 @@ def plot_filtered_boxplot(df):
     sns.boxplot(data=df)
     plt.xticks(rotation=90)
     plt.tight_layout()
-    plt.show()
+    plt.savefig('./outputs/filtered_boxplot.png')
+    plt.close()
 
 # Genera una matriz de dispersión (scatter matrix) de las características seleccionadas
 def plot_scatter_matrix(df, feature_columns, target_column):
@@ -50,7 +57,8 @@ def plot_scatter_matrix(df, feature_columns, target_column):
     [s.get_yaxis().set_label_coords(-0.9,0.5) for s in sm.reshape(-1)]
     [s.set_xticks(()) for s in sm.reshape(-1)]
     [s.set_yticks(()) for s in sm.reshape(-1)]
-    plt.show()
+    plt.savefig('./outputs/scatter_matrix.png')
+    plt.close()
 
 # Genera un heatmap de correlación entre las características del DataFrame
 def plot_correlation_heatmap(df):
@@ -60,14 +68,17 @@ def plot_correlation_heatmap(df):
     ax = sns.heatmap(corrMat, vmax=1, vmin=-1, cbar_kws={"shrink": .8}, square=True, annot=True, fmt='.2f', cmap='GnBu', center=0)
     bottom, top = ax.get_ylim()
     ax.set_ylim(bottom + 0.5, top - 0.5)
-    plt.show()
+    plt.savefig('./outputs/correlation_heatmap.png')
+    plt.close()
 
 # Genera un gráfico de coordenadas paralelas para visualizar las relaciones entre las características y la variable objetivo
 def plot_parallel_coordinates(df, target_column):
     parallel_coordinates(df, target_column, colormap='cool', xticks=None)
-    plt.show()
+    plt.savefig('./outputs/parallel_coordinates.png')
+    plt.close()
 
 # Genera curvas de Andrews para visualizar la distribución de las características en función de la variable objetivo
 def plot_andrews_curves(df, target_column):
     andrews_curves(df, target_column, colormap='rainbow')
-    plt.show()
+    plt.savefig('./outputs/andrews_curves.png')
+    plt.close()
